46194ee18de3a4c1cda7f4aabbbb0a61b4e14f82
az-badri <badrtdinoff.az27@gmail.com>
2025-12-12 19:35:40 +0300

    feat: added basic folder structure + navigation files configured + prettier/lint

---
+++ src/navigation/RootNavigator.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BottomTabs from './BottomTabs';
+
+export type RootStackParamList = {
+    Tabs: undefined;
+    Notifications: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function RootNavigator() {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Tabs" component={BottomTabs} />
+           {/* <Stack.Screen name="Notifications" component={NotificationsScreen} />
+            */}
+        </Stack.Navigator>
+    );
+}
